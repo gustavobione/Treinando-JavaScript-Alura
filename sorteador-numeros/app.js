fef2c3f67a76247aefd9c187d2784a91d2594353
Gustavo <gustavoteixeira394@gmail.com>
2025-01-06 17:04:41 -0300

    função de sortear numero

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -3,8 +3,21 @@ function sortear() {
     let de = document.getElementById('de').value;
     let ate = document.getElementById('ate').value;
 
-    alert(`Quantidade: ${quantidade}`);
-    alert(`Do número: ${de}`);
-    alert(`Até o número: ${ate}`);
-    
+    let sorteados = [];
+    let numero;
+
+    for (let i = 0; i < quantidade; i++){
+        numero = obterNumeroAleatorio(de, ate);
+        sorteados.push(numero);
+    }
+
+    alert(sorteados);
+}
+
+function obterNumeroAleatorio(min, max) {
+    const minNumero = Math.ceil(min);
+    const maxNumero = Math.floor(max);
+    let resultado = Math.floor(Math.random() * (maxNumero - minNumero + 1)+ minNumero) ;
+    return resultado;
+
 }
