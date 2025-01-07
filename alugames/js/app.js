ba2bae17703763d2839aa902e2ac5a16069f49fc
Gustavo <gustavoteixeira394@gmail.com>
2025-01-07 16:28:53 -0300

    aluGames- Contagem de alugados

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -1,3 +1,10 @@
+let jogosAlugados = 0;
+
+
+function contarEExibirJogosAlugados() {
+    console.log(`Total de jogos alugados: ${jogosAlugados}`);
+}
+
 function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
@@ -9,10 +16,19 @@ function alterarStatus(id) {
         imagem.classList.remove('dashboard__item__img--rented');
         botao.classList.remove('dashboard__item__button--return');
         botao.textContent = 'Alugar';
+        jogosAlugados--;
         }
     } else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
         botao.textContent = 'Devolver';
+        jogosAlugados++;
     }
+
+    contarEExibirJogosAlugados();
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+    contarEExibirJogosAlugados();
+});
