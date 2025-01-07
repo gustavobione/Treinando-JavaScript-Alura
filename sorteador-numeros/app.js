a52e9c83274a06afc9d73065bd416eb57924dfe8
Gustavo <gustavoteixeira394@gmail.com>
2025-01-07 14:46:36 -0300

    função de reset

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -8,15 +8,17 @@ function sortear() {
 
     for (let i = 0; i < quantidade; i++){
         numero = obterNumeroAleatorio(de, ate);
+
+        while (sorteados.includes(numero)) {
+            numero = obterNumeroAleatorio(de, ate);
+        }
+
         sorteados.push(numero);
     }
 
     let resultado = document.getElementById('resultado');
-    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados} </label>`
-
-    while (sorteados.includes(numero)) {
-        numero = obterNumeroAleatorio(de,ate);
-    }
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
+    alterarStatusBotao();
 }
 
 function obterNumeroAleatorio(min, max) {
@@ -27,4 +29,22 @@ function obterNumeroAleatorio(min, max) {
 
 }
 
+function alterarStatusBotao() {
+    let botao = document.getElementById('btn-reiniciar');
+    if (botao.classList.contains('container__botao-desabilitado')) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>';
+    alterarStatusBotao();
+}
 
