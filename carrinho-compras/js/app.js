6026a98814a07f8b9b9cd88009b55f86378ebf4c
Gustavo <gustavoteixeira394@gmail.com>
2025-01-09 15:11:15 -0300

    Carrinho-utilização de split e soma

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,11 +1,18 @@
 function adicionar() {
-    
-    let nomeProduto = document.getElementById('produto');
+
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
     let quantidade = document.getElementById('quantidade');
     let valor = document.getElementById('valor');
 
-    alert(nomeProduto.value);
+    alert(nomeProduto);
+    alert(valorUnitario);
     alert(quantidade.value);
+
+    let preco = quantidade.value * valorUnitario;
+
+    alert(preco);
 
 }
 
