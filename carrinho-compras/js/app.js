ce42821f08def21b1b944c7a98c3796861827048
Gustavo <gustavoteixeira394@gmail.com>
2025-01-09 14:54:31 -0300

    Carrinho-criação das função de quantidade e valor

---
+++ carrinho-compras/js/app.js
@@ -0,0 +1,14 @@
+function adicionar() {
+    
+    let nomeProduto = document.getElementById('produto');
+    let quantidade = document.getElementById('quantidade');
+    let valor = document.getElementById('valor');
+
+    alert(nomeProduto.value);
+    alert(quantidade.value);
+
+}
+
+function limpar() {
+
+}
