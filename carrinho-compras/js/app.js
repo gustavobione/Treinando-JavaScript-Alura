62f1730ceeef8e73a63959b2d90aebb85178dc23
Gustavo <gustavoteixeira394@gmail.com>
2025-01-09 15:43:40 -0300

    Carrinho - valor total e limpar carrinho

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,21 +1,28 @@
+let totalGeral;
+limpar();
+
 function adicionar() {
 
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
     let valorUnitario = produto.split('R$')[1];
-    let quantidade = document.getElementById('quantidade');
+    let quantidade = document.getElementById('quantidade').value;
     let valor = document.getElementById('valor');
+    let preco = quantidade * valorUnitario;
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+  </section>`;
+    totalGeral = totalGeral + preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${totalGeral}`;
 
-    alert(nomeProduto);
-    alert(valorUnitario);
-    alert(quantidade.value);
-
-    let preco = quantidade.value * valorUnitario;
-
-    alert(preco);
 
 }
 
 function limpar() {
+    totalGeral = 0
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').innerHTML = 'R$ 0';
 
 }
